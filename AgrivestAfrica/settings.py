535fa1cf3bdb042d8e853ba753a1ed3ac83495e6
alphaDinar <yanksone18@yahoo.com>
2022-12-27 09:06:40 +0000

    mmm

--- AgrivestAfrica/settings.py
+++ AgrivestAfrica/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['agrivestafrica-production.up.railway.app','www.agrivestafrica.com']
+ALLOWED_HOSTS = ['agrivestafrica-production.up.railway.app','www.agrivestafrica.com','agrivestafrica.com']
 AUTH_USER_MODEL = 'Log.User'
 
 CSRF_TRUSTED_ORIGINS = ['https://agrivestafrica-production.up.railway.app','https://www.agrivestafrica.com']
