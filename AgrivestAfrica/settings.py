84f29698ebbba7fd1c04867fb617c0eda4c6e39d
alphaDinar <yanksone18@yahoo.com>
2022-12-27 11:55:08 +0000

    mmm

--- AgrivestAfrica/settings.py
+++ AgrivestAfrica/settings.py
@@ -88,25 +88,25 @@ WSGI_APPLICATION = 'AgrivestAfrica.wsgi.application'
 
 
 
-# DATABASES = {
-#         'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'ACvinPt4J0h228UgjAhr',
-        'HOST': 'containers-us-west-66.railway.app',
-        'PORT': '6274'
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
- }
+}
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'ACvinPt4J0h228UgjAhr',
+#         'HOST': 'containers-us-west-66.railway.app',
+#         'PORT': '6274'
+#     }
+#  }
 
 
 # Password validation
