6bf42e13efd7eeaa6d6595512c9f2820cdc6a80f
alphaDinar <yanksone18@yahoo.com>
2023-01-14 12:02:39 +0000

    change

--- AgrivestAfrica/settings.py
+++ AgrivestAfrica/settings.py
@@ -16,8 +16,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['agrivestafrica-production.up.railway.app','www.agrivestafrica.com','agrivestafrica.com']
-# ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['agrivestafrica-production.up.railway.app','www.agrivestafrica.com','agrivestafrica.com']
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'Log.User'
 
