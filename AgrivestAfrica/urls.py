a98ee7108b75c6ae132379f10f01be3f025b6100
alphaDinar <yanksone18@yahoo.com>
2023-03-16 22:32:37 +0000

    change

--- AgrivestAfrica/urls.py
+++ AgrivestAfrica/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-import debug_toolbar
+# import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,7 +10,7 @@ urlpatterns = [
     path('',include('Log.urls')),
     path('',include('Transaction.urls')),
     path('',include('allauth.urls')),
-    path('__debug__/', include('debug_toolbar.urls')),
+    # path('__debug__/', include('debug_toolbar.urls')),
     path('robots.txt', include('robots.urls')),
 ]
 
