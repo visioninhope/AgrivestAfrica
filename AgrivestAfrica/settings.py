3068ff3e6ee38d663c157a69df86d41bd93ec08a
alphaDinar <yanksone18@yahoo.com>
2023-03-26 12:21:56 +0000

    vc

--- AgrivestAfrica/settings.py
+++ AgrivestAfrica/settings.py
@@ -148,7 +148,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
 
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
